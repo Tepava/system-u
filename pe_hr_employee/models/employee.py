502a0ecc7c171d873fe4ff7a512bd48523d20c88
Tepava <mehdi@pacific-erp.com>
2022-05-02 23:58:23 +0000

    [UPDATE]pe_account_payment_register | [REMOVE]Remove pe_hr_employee

--- pe_hr_employee/models/employee.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class pe_hr_employee(models.Model):
-    _inherit = 'hr.employee'
-    
-    departure_reason = fields.Selection(selection_add=[('contract_end', 'Fin de contrat')])
--- pe_hr_employee/models/employee_wizard.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class pe_hr_employee_wizard(models.TransientModel):
-    _inherit ='hr.departure.wizard'
-
-    departure_reason = fields.Selection(selection_add=[('contract_end', 'Fin de contrat')])
