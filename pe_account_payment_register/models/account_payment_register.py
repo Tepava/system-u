1b9728ea37712582fcb84b12667ab92279da8ab3
Tepava <mehdi@pacific-erp.com>
2022-05-03 01:11:01 +0000

    [UPDATE] pe_account_payment_register

--- pe_account_payment_register/models/account_payment_register.py
+++ pe_account_payment_register/models/account_payment_register.py
@@ -11,29 +11,10 @@ class AccountPaymentRegister(models.TransientModel):
 #     ID des cheques 9 e
 #     action_create_payments = action de création d'un paiement
 #     _create_payment_vals_from_wizard = création d'un paiement à partir du wizard
-
+    
     def _create_payment_vals_from_wizard(self):
-        payment_vals = {
-            'date': self.payment_date,
-            'amount': self.amount,
-            'payment_type': self.payment_type,
-            'partner_type': self.partner_type,
-            'ref': self.communication,
-            'journal_id': self.journal_id.id,
-            #Ajout du numéros de chèque
-            'cheque_number': self.cheque_number,
-            'currency_id': self.currency_id.id,
-            'partner_id': self.partner_id.id,
-            'partner_bank_id': self.partner_bank_id.id,
-            'payment_method_line_id': self.payment_method_line_id.id,
-            'payment_method_id': self.payment_method_id.id,
-            'destination_account_id': self.line_ids[0].account_id.id
-        }
-
-        if not self.currency_id.is_zero(self.payment_difference) and self.payment_difference_handling == 'reconcile':
-            payment_vals['write_off_line_vals'] = {
-                'name': self.writeoff_label,
-                'amount': self.payment_difference,
-                'account_id': self.writeoff_account_id.id,
-            }
+        # OVERRIDE
+        # Ajout du numéros de chèque
+        payment_vals = super()._create_payment_vals_from_wizard()
+        payment_vals['cheque_number'] = self.cheque_number
         return payment_vals
