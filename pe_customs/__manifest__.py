34ad154abe628aefbae31c553eb20da3250dcfe7
Tepava <mehdi@pacific-erp.com>
2022-05-10 10:53:18 -1000

    Upgrade pe_custom (farenatura)

---
+++ pe_customs/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Modification apporté a TE FARE NATURA (Pacific-ERP)",
+    "summary": "Petits modifications faites sur la base (vue,fields,action,etc...)",
+    "version": "0.1",
+    "category": "Pacific-ERP",
+    "author": "Mehdi Tepava",
+    'website': "https://www.pacific-erp.com/",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["event"],
+    "data": [
+        "views/event_ticket_print.xml"
+        ],
+}
